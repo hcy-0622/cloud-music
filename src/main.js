eb048f62226b21bde519bd037f239441bb024e39
Lyric <hcy_0622@163.com>
2021-05-27 12:54:59 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
+import fastclick from 'fastclick'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 new Vue({
   router,
   store,
